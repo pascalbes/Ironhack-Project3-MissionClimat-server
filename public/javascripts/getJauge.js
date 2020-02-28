4a91e02e4766b53e7b49d1d8daaf2e9deadf51f7
Paul Carillion <pcarillion@yahoo.fr>
2020-02-28 16:22:26 +0100

    data ready for jauge and pie

--- public/javascripts/getJauge.js
+++ public/javascripts/getJauge.js
@@ -95,73 +95,45 @@ function main(auth) {
     range: resultsRange,
     }, (err, res) => {
 
+
         var rows = res.data.values;
 
-        // console.log(rows)
-
-        var i=13
-        var j=0
-
-        console.log(rows[i][j])
-
-        var datas = {}
-        datas.data02=[]
-
-        function createData02() {for (let i = 14; i < 24; i ++) {
-          datas.data02.push({
-            name: rows[i][3],
-            value: rows[i][5]
-          })
-        }}
-
-        createData02()
-
-        datas.data01 = []
-
-        var listSectors = []
-        function createListSectors() {
-          {for (let i = 14; i < 24; i ++) {
-            if (!listSectors.includes(rows[i][2])) listSectors.push(rows[i][2])
-          }}
-          return listSectors
-        }
-
-        createListSectors()
-
-        console.log(listSectors)
-
-        function createObjectForDatas() {
-          for (let i = 0; i < listSectors.length; i++){
-            datas.data01.push(
-              {name : listSectors[i],
-                value: 0}
-            )
-          }
-        }
-
-        createObjectForDatas()
+        var datas = []
 
         function formatNombre(nb) {
             var number = Number(nb.toString().replace(",", "."))
             return number
         }
 
-        function calculateSectorsValue() {
-          for (let i = 14; i < 24; i ++){
-            for (let x = 0; x < 6; x ++) {
-              if (rows[i][2] === datas.data01[x].name) {
-                datas.data01[x].value += formatNombre(rows[i][5]) 
-              }
+
+        function createObjectForEachSector() {
+            for (i =81; i < 88; i ++){
+                datas.push(
+                    [{
+                        "name": rows[i][2],
+                        "id": "MGT de CO2",
+                        "ranges":[
+                            formatNombre(rows[i][5]),
+                            formatNombre(rows[i][6]),
+                            formatNombre(rows[i][4])
+                        ],
+                        "measures":[
+                            formatNombre(rows[i][3])
+                        ],
+                        "markers":[
+                            formatNombre(rows[i][5]),
+                            formatNombre(rows[i][6])
+                        ]
+                    }]
+                )
             }
-          } return datas.data01
         }
 
-        calculateSectorsValue()
+        createObjectForEachSector()
 
-        console.log(datas.data01)
+        console.log(datas[1][0].ranges, datas[1][0].markers)
+        
 
-        console.log(datas)
-
-    });
+});
 
 }
