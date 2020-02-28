9423209dfa88a563cfcc107baf3c17f34bf940f8
Paul Carillion <pcarillion@yahoo.fr>
2020-02-28 16:29:56 +0100

    pie

--- public/javascripts/getResultTest2.js
+++ public/javascripts/getResultTest2.js
@@ -104,7 +104,6 @@ function main(auth) {
 
 
         var datas = {}
-<<<<<<< HEAD
         datas.data02=[]
 
         function createData02() {for (let i = 14; i < 24; i ++) {
@@ -159,15 +158,6 @@ function main(auth) {
         calculateSectorsValue()
 
         console.log(datas.data01)
-=======
-        datas.data=[]
-
-        datas.title = rows[i+0][j+1]
-        datas.yTitle = rows[i+1][j+1]
-
-        j=3;
-
->>>>>>> 87ef69de26fe091fa8ba8840b4ab8cfd4ad1ff67
 
         console.log(datas)
 
