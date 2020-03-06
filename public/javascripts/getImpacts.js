200b1938dd528ab6bb706705e546071a067ebcaa
Pascal Besson <pascalbes@gmail.com>
2020-03-06 10:38:57 +0100

    temperature  number

--- public/javascripts/getImpacts.js
+++ public/javascripts/getImpacts.js
@@ -1,7 +1,21 @@
+function formatNumber(dat, isPercent) {
+  
+    //cas ou dat est un nombre
+    if (!isNaN(Number(dat.replace(",",".")))) {
+      var numberFormated = Number(dat.replace(",", "."))
+      isPercent == 1 ? numberFormated *= 100 : "kikou"
+      return numberFormated
+    }
+    else {
+      return dat
+    }
+
+}
+
 function getImpacts(rows, i,j) {
     
     var data = {
-        temperature: rows[i][4],
+        temperature: formatNumber(rows[i][4],0),
         temperatureRange: rows[i+1][4],
         RCP: rows[i+2][4],
         jours35: rows[i+3][4],
