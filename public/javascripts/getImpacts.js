7e2ac84c74df833bbf5595cf5fe817190c81621d
Pascal Besson <pascalbes@gmail.com>
2020-03-06 09:05:18 +0100

    jauge done

--- public/javascripts/getImpacts.js
+++ public/javascripts/getImpacts.js
@@ -2,10 +2,11 @@ function getImpacts(rows, i,j) {
     
     var data = {
         temperature: rows[i][4],
-        RCP: rows[i+1][4],
-        jours35: rows[i+2][4],
-        joursSecheresse: rows[i+3][4],
-        reductionEmission2030: rows[i+5][4]
+        temperatureRange: rows[i+1][4],
+        RCP: rows[i+2][4],
+        jours35: rows[i+3][4],
+        joursSecheresse: rows[i+4][4],
+        reductionEmission2030: rows[i+6][4]
     } 
   
     return data
