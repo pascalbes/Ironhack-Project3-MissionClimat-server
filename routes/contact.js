c5e2096b4805f1be71a420bcf0e83a4d6cd8ab76
Pascal Besson <pascalbes@gmail.com>
2020-04-26 23:56:08 +0200

    updates

--- routes/contact.js
+++ routes/contact.js
@@ -2,56 +2,42 @@ const express = require("express");
 const router = new express.Router();
 require("dotenv").config();
 
-
+var nodemailer = require('nodemailer');
 // HELP JE SAIS PAS COMMENT CONNECTER BIEN LES DEUX 
 
+router.post("/send", (req, res, next) => {
 
+    console.log(req.body)
 
-var nodemailer = require('nodemailer');
-
-var transport = {
-    host: 'smtp.mail.com', // can be changed
-    auth: {
-        user: process.env.SENDMAIL, //can be changed
-        pass: process.env.SENDPWD // can be changed
-    }
-}
-
-var transporter = nodemailer.createTransport(transport)
-
-transporter.verify((error, success) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Server is ready to take messages');
-    }
-});
-
-router.post("/contact", (req, res, next) => {
-    var name = req.body.name
-    var email = req.body.email
-    var topic = req.body.topic
-    var message = req.body.message
-    var content = `name: ${name} \n email: ${email} \n topic: ${topic} \n message: ${message}`
-
-    var mail = {
-        from: name,
-        to: 'mission-climat@mail.com', //can be changed
-        subject: topic,
-        text: content
-    }
-
-    transporter.sendMail(mail, (err, data) => {
-        if (err) {
-            res.json({
-                msg: 'fail'
-            })
-        } else {
-            res.json({
-                msg: 'success'
-            })
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.mail.com', // can be changed
+        secure: false, // use SSL
+        port: 25, // port for secure SMTP
+        auth: {
+            user: process.env.SENDMAIL, //can be changed
+            pass: process.env.SENDPWD // can be changed
+        },
+        tls: {
+            rejectUnauthorized: false
         }
-    })
+      });
+      
+      var mailOptions = {
+        from: 'machin@gmail.com',
+        to: 'mission-climat@mail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.status(200).json({ msg: 'success' })
+        }
+      });
+
 })
 
 module.exports = router;
