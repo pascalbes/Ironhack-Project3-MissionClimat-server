ee893aed142eb22dfd08ab2bae9fd664df58eac8
Pascal Besson <pascalbes@gmail.com>
2020-04-26 12:51:49 +0200

    new route for init

--- routes/gsheet.js
+++ routes/gsheet.js
@@ -41,53 +41,53 @@ router.get("/download/:id", (req, res, next) => {
 
 
 
-//route permettant de récupérer les valeurs des paramètres d'une spreadsheet déjà créée
-  router.get("/values/:id", (req, res, next) => {
+// //route permettant de récupérer les valeurs des paramètres d'une spreadsheet déjà créée
+//   router.get("/values/:id", (req, res, next) => {
   
-    // If modifying these scopes, delete token.json.
-  const SCOPES = ['https://www.googleapis.com/auth/drive'];
+//     // If modifying these scopes, delete token.json.
+//   const SCOPES = ['https://www.googleapis.com/auth/drive'];
   
-    function formatNumber(number, isPercent) {
-      var numberFormated = Number(number.replace(",", "."))
-      isPercent == 1 ? numberFormated *= 100 : "kikou"
-      return numberFormated
-    }
+//     function formatNumber(number, isPercent) {
+//       var numberFormated = Number(number.replace(",", "."))
+//       isPercent == 1 ? numberFormated *= 100 : "kikou"
+//       return numberFormated
+//     }
   
-    async function main () {
-        // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
-        // environment variables.
-        const auth = new google.auth.GoogleAuth({
-          // Scopes can be specified either as an array or as a single, space-delimited string.
-          scopes: ['https://www.googleapis.com/auth/drive']
-        });
+//     async function main () {
+//         // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
+//         // environment variables.
+//         const auth = new google.auth.GoogleAuth({
+//           // Scopes can be specified either as an array or as a single, space-delimited string.
+//           scopes: ['https://www.googleapis.com/auth/drive']
+//         });
 
-        const idSheet=req.params.id
-        const rangeParams = 'Paramètres!F3:J37'
+//         const idSheet=req.params.id
+//         const rangeParams = 'Paramètres!F3:J37'
 
-        const sheets = google.sheets({version: 'v4', auth});
+//         const sheets = google.sheets({version: 'v4', auth});
     
-        sheets.spreadsheets.values
-        .get({
-            spreadsheetId: idSheet,
-            range: rangeParams,
-        })
-        .then(response => {
+//         sheets.spreadsheets.values
+//         .get({
+//             spreadsheetId: idSheet,
+//             range: rangeParams,
+//         })
+//         .then(response => {
 
-            var rows=response.data.values
-            var values = []
+//             var rows=response.data.values
+//             var values = []
 
-            rows.forEach(row => {
-            !isNaN(Number(row[4].replace(",","."))) ? values.push([formatNumber(row[4],row[0]==="%")]) : values.push([row[4]])
-            })
+//             rows.forEach(row => {
+//             !isNaN(Number(row[4].replace(",","."))) ? values.push([formatNumber(row[4],row[0]==="%")]) : values.push([row[4]])
+//             })
 
-            res.status(200).json({ values: values})
-        })
-        .catch(res.status(500))
+//             res.status(200).json({ values: values})
+//         })
+//         .catch(res.status(500))
 
-    }
-    main().catch(res.status(500))
+//     }
+//     main().catch(res.status(500))
   
-  })
+//   })
 
 
 //copie de la spreadsheet master. Renvoie l'ID de la copie
@@ -208,6 +208,41 @@ router.get("/", (req, res, next) => {
     main().catch(res.status(500))
   
   })
+
+
+  //actualisation de la sheet avec de nouveaux paramètres et c'est tout (utile pour la réinite)
+  router.patch("/updateonly/:id", (req, res, next) => {
+  
+    const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+
+    async function main () {
+        // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIALS
+        // environment variables.
+        const auth = new google.auth.GoogleAuth({scopes: SCOPES})
+  
+        const idSheet=req.params.id
+        const values=req.body.values
+        const rangeParams = 'Paramètres!J3:J37'
+
+        const sheets = google.sheets({version: 'v4', auth});
+    
+        sheets.spreadsheets.values.update({
+            spreadsheetId: idSheet,
+            range: rangeParams,
+            valueInputOption: 'RAW',
+            "resource": {
+            "values": values
+            }
+            })
+        .then(response => {  
+          res.status(200).json({ response:"done"})
+        })
+        .catch(res.status(500))
+    }
+    main().catch(res.status(500))
+  
+  })
+
 
   router.delete("/delete/:id", (req, res, next) => {
   
