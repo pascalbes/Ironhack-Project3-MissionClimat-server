755439832c1362dabfb792ee3890b831dcd2347b
Pascal Besson <pascalbes@gmail.com>
2023-06-29 23:08:52 +0200

    update app

--- app.js
+++ app.js
@@ -12,21 +12,16 @@ const passport = require("passport"); // auth library (needs sessions)
 
 var app = express();
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+if (process.env.NODE_ENV !== 'development') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }))
+};
 app.use(logger("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 
-app.use(
-  session({
-    cookie: { secure: false, maxAge: 4 * 60 * 60 * 1000 }, // 4 hours
-    resave: true,
-    saveUninitialized: true,
-    secret: process.env.SECRET_SESSION,
-  })
-);
+
 
 var corsOptions = {
   origin: [process.env.FRONTEND_URI, process.env.FRONTEND_URL_SECURE],
@@ -45,19 +40,8 @@ app.use(
     secret: process.env.SECRET_SESSION,
   })
 );
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URI);
-//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 
 app.use("/sheet", require("./routes/gsheet"));
-
-app.use("/email", require("./routes/email"));
 
 app.use("*", (req, res, next) => {
   console.log("here");
