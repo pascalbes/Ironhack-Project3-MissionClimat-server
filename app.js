44ebabe35d48874bbd27ac9a084475cc7af6b047
Pascal Besson <pascalbes@gmail.com>
2023-06-29 21:53:47 +0200

    retry

--- app.js
+++ app.js
@@ -16,18 +16,9 @@ const passport = require("passport"); // auth library (needs sessions)
 var app = express();
 
 app.use(logger("dev"));
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 
 app.use(
@@ -48,14 +39,20 @@ var corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "/public")));
 
+app.use(
+  session({
+    cookie: { secure: false, maxAge: 4 * 60 * 60 * 1000 }, // 4 hours
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SECRET_SESSION,
+  })
+);
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URI);
 //   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //   next();
 // });
 
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.send("hello world");
